2541e1c0ecfed6a29bd57e5450b9b229c4c5d1e5
zihan167 <152907214+zihan167@users.noreply.github.com>
2024-03-11 03:47:00 +0800

    Add files via upload

---
+++ hw1-1_ntustM11207503.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 創建神經網路模型
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet, self).__init__()
+        self.fc = nn.Linear(1, 1)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
+
+# 初始化數據
+x_train = torch.FloatTensor([[0], [1]])
+y_train = torch.FloatTensor([[0], [1]])
+
+# 定義模型、損失函數和優化器
+model = SimpleNet()
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+
+# 訓練模型
+epochs = 40
+losses = []
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    outputs = model(x_train)
+    loss = criterion(outputs, y_train)
+    loss.backward()
+    optimizer.step()
+    losses.append(loss.item())
+
+    if (epoch+1) % 5 == 0:
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item()}')
+
+# 繪製曲線
+plt.plot(range(epochs), losses)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Training Loss Convergence')
+plt.show()
+
+# 權重、輸出
+print("Weights of the network:")
+print(model.fc.weight)
+print("\nOutputs for input (0, 0) and (1, 1):")
+with torch.no_grad():
+    output_00 = model(torch.FloatTensor([[0]]))
+    output_11 = model(torch.FloatTensor([[1]]))
+print("Output for (0, 0):", output_00.item())
+print("Output for (1, 1):", output_11.item())
